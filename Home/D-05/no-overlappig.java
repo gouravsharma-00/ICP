fe1ef7d0006d61abc68ea251eedee2ba4ab75860
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2025-10-01 11:00:00 +0000

    Home Assignment 2

--- Home/D-05/no-overlappig.java
+++ Home/D-05/no-overlappig.java
@@ -1,2 +1,24 @@
 // 435. Non-overlapping Intervals
 
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int b[]) {
+                return a[1] - b[1];
+            }
+        });
+
+        int end = intervals[0][1];
+        int count = 1;
+
+        for(int i = 1; i < intervals.length; i++) {
+            if(intervals[i][0] >= end) {
+                count++;
+                end = intervals[i][1];
+            }
+        }
+
+        return intervals.length - count;
+    }
+}
