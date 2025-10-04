5966381617e6229f7c7d7fafa9646358106fedc9
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2025-10-04 17:34:11 +0000

    Home Assignment 7

---
+++ Home/D-07/house-robbers.java
@@ -0,0 +1,22 @@
+// 198. House Robber
+
+import java.util.*;
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return Robber(nums, 0, dp);
+    }
+    public int Robber(int[] nums, int i, int[] dp) {
+        if(i >= nums.length) {
+            return 0;
+        }
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        int rob = nums[i] + Robber(nums, i + 2, dp);
+        int dont_rob = Robber(nums, i + 1, dp);
+
+        return dp[i] = Math.max(rob, dont_rob);
+    }
+}
